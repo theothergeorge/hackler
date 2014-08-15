cef3d1bee436bf5e4418d833e5e92ac4de6364ae
Luke Ma <luke@bluejeans.com>
2014-08-15 11:18:04 -0700

    Remind people to login using bluejeansnet.com. Also added a schedule list for people to see.

---
+++ public/scripts/schedule.js
@@ -0,0 +1,21 @@
+var START = 3 * 60 + 15;
+function getTimeString(time) {
+    var actualTime = START + time;
+    var minutes = Math.floor(actualTime / 60);
+    var seconds = actualTime % 60;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    return minutes + " : " + seconds;
+}
+
+$(document).ready(function() {
+	var presentations = window.presentations;
+
+	var container = $('#scheduleList');
+    for (var i =0; i < presentations.length; i++) {
+        var presentation = presentations[i];
+        container.append('<li id="' + presentation.teamId + '">' +
+            '<span class="teamName">' + getTimeString(i * 5) + " - " +
+            presentation.name + '</span><br/>' +
+        '</li>');
+    }
+ });
